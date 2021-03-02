2949e221ea59e079a7e4a83c738cf606e59bc5b1
Matthew Bonebrake <matthewcbonebrake@gmail.com>
2021-03-02 01:32:18 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  return num2 + num1
 end
+p addition(3, 5)
 
 def subtraction(num1, num2)
-
+  return num1 - num2
 end
+p subtraction(3, 6)
 
 def division(num1, num2)
-
+  return num1 / num2
 end
+p division(3, 6)
 
 def multiplication(num1, num2)
-
+  return num1 * num2
 end
+p multiplication(4, 5)
 
 def modulo(num1, num2)
-
+  return num1 % num2
 end
+p modulo(8,9)
 
 def square_root(num)
-
+  return Math.sqrt(num)
 end
+p square_root(25)

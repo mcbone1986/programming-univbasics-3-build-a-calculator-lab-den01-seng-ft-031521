66d8ce04b2e9fd0791b4b068ba163aefdf0a05d2
Matthew Bonebrake <matthewcbonebrake@gmail.com>
2021-03-02 01:32:01 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,29 +1,29 @@
 def addition(num1, num2)
   return num2 + num1
 end
-addition(3, 5)
+p addition(3, 5)
 
 def subtraction(num1, num2)
   return num1 - num2
 end
-subtraction(3, 6)
+p subtraction(3, 6)
 
 def division(num1, num2)
   return num1 / num2
 end
-division(3, 6)
+p division(3, 6)
 
 def multiplication(num1, num2)
   return num1 * num2
 end
-multiplication(4, 5)
+p multiplication(4, 5)
 
 def modulo(num1, num2)
   return num1 % num2
 end
-modulo(8,9)
+p modulo(8,9)
 
 def square_root(num)
   return Math.sqrt(num)
 end
-square_root(25)
+p square_root(25)
